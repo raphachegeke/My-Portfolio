f1d203de0fa8e6d086349e3730c1818007f5db3a
raphachegeke <raphackamunu@gmail.com>
2025-02-27 10:39:06 +0300

    Updated portfolio with new files

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-  const menu = document.querySelector('.menu');
-
-  hamburger.addEventListener('click', () => {
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-    menu.style.flexDirection = 'column';
-  });
